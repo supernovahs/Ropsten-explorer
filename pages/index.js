ed4eb33cb74c1a593fca41baa1a3a56679e9c6e4
supernovahs <harshitsinghal252@gmail.com>
2022-06-07 02:55:21 +0530

    WIP : Ropsten simple explorer

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
+const ethers = require("ethers");
+
 
 export default function Home() {
+  const [Address, setAddress] = useState('');
+  const [balance, setbalance] = useState();
+  const [iscontract, setiscontract] = useState(null);
+  const [nonce, setnonce] = useState(null);
+  const [blocknumber, setblocknumber] = useState(null);
+  console.log("iscontract", iscontract);
+  const providers = new ethers.providers.JsonRpcProvider("https://ropsten.infura.io/v3/d21c9a0af06049d980fc5df2d149e4bb");
+  console.log("providers", providers);
+  const getTx = async (address) => {
+    const length = await providers.getCode(address);
+    console.log("length", length);
+    if (length.length > 0) {
+      setiscontract(true);
+    }
+    console.log(iscontract);
+    const bal = await providers.getBalance(address);
+    setbalance(ethers.BigNumber.from(bal).toString());
+
+    const non = await providers.getTransactionCount(address);
+    setnonce(non);
+    const blocknum = await providers.getBlockNumber();
+    setblocknumber(blocknum);
+
+
+
+  }
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,42 +44,35 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Ropsten Block Explorer
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <input
+            placeholder="Enter address"
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <button
+              onClick={() => {
+                getTx(Address);
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              }}
+
+            >
+              Click
+            </button>
+          </div>
+
+
+        </div>
+        <div>
+          <h2>Balance: {(balance / 10 ** 18).toFixed(2)}  ETH</h2 >
+          {<h2>Contract : {iscontract != null ? iscontract : ""}</h2>}
+          <h2>Nonce: {nonce}</h2>
+          <h2>BlockNumber: {blocknumber}</h2>
         </div>
       </main>
 
